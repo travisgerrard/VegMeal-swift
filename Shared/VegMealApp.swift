5c1b48b7505759155db8a972299578727e1f2622
Travis Gerrard <travisgerrard@gmail.com>
2020-08-21 07:29:05 -0700

    Initial Commit

---
+++ Shared/VegMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  VegMealApp.swift
+//  Shared
+//
+//  Created by Travis Gerrard on 8/21/20.
+//
+
+import SwiftUI
+
+@main
+struct VegMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
